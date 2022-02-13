db48a7f839eee62752031722ab6824d4afb77f5c
Achudan <achuts4@gmail.com>
2022-02-12 20:44:17 -0500

    ingredients data file structure

--- src/pages/IngredientsPage/ingredients-data.js
+++ src/pages/IngredientsPage/ingredients-data.js
@@ -1,20 +1,10 @@
-const INGREDIENTS_DATA = [
-    {
-        "ingredientName" : "chicken_pesto_parm",
+const INGREDIENTS_DATA = {
+    "chicken_pesto_parm" : {
         "bases" : ['spinach', 'penne', 'orgula', 'rice'],
         "toppings": ['jalapeno', 'peas', 'mushrooms', 'brocolli', 'lentils'],
         "sauces" : ['mayonaisse', 'sour creme', 'hot sauce'],
-        "proteins" : ['chicken', 'bean'],
-        "selected" : ['spinach', 'peas']
-    },
-    {
-        "ingredientName" : "chicken_salad",
-        "bases" : ['spinach', 'penne'],
-        "toppings": ['jalapeno', 'peas'],
-        "sauces" : ['mayonaisse', 'sour creme', 'hot sauce'],
         "proteins" : ['chicken', 'bean']
     }
-
-]
+}
 
 export default INGREDIENTS_DATA;
