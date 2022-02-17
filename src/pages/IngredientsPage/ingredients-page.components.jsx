9c9dbba49248b04c7a44c751c1cc95f417a53675
Achudan <achuts4@gmail.com>
2022-02-16 21:35:35 -0500

    items

--- src/pages/IngredientsPage/ingredients-page.components.jsx
+++ src/pages/IngredientsPage/ingredients-page.components.jsx
@@ -78,7 +78,7 @@ const IngredientsPage = () => {
             <div className="product-details">
                 <h1>{ingredientList[ingredientName].name}</h1>
                 {/* <div> */}
-                <span className="heading">Item in the bowl</span>
+                <span className="heading">Items in the bowl</span>
                 <ul className="selected-list">
                     {
                         arr.map((item, index)=>{
