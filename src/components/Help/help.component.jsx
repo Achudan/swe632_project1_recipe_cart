1292365744dce758a4b174b7bb6ae35e7e496083
Ishwarya Lakshmi <ishwarya.lakshmi.b@gmail.com>
2022-04-27 16:20:55 -0400

    Help and documentation

--- src/components/Help/help.component.jsx
+++ src/components/Help/help.component.jsx
@@ -2,12 +2,27 @@ import React from "react";
 
 const Help = () => (
     <div>
-        <h1>How do I place an online order</h1>
+    <div>
+        
+
+
+        <h1>Steps to place order at Taste Buds</h1>
+        <h3>1. Choose from the list</h3>
+        <h3>2. Customize to taste</h3>
+        <h3>3. Check out the cart</h3>
+
+        <img src={require('./help-and-docs.gif')  } alt="Loading"></img>
+        </div>
+
+
+
+<div>
+        <h4>What we do at Taste Buds</h4>
         <p>Taste Buds provide various recipes that you can try out. You can place order on the recipes and you will be able to buy individual items. </p>
         <p>At taste buds we allow you to build your own recipe while also choosing a range of recipes from our menu.</p>
         <p></p>
     </div>
-
+    </div>
 )
 
 export default Help;
