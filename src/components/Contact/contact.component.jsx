124705fdfcf13aabe65d8764549cf1805401b719
Achudan <achuts4@gmail.com>
2022-04-27 07:21:36 -0400

    contact icons

--- src/components/Contact/contact.component.jsx
+++ src/components/Contact/contact.component.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import './contact.style.scss';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { MdCall } from 'react-icons/md';
+import { FaFacebook } from 'react-icons/fa'
 
 const Contact = () => (
     <div>
@@ -6,7 +10,12 @@ const Contact = () => (
         <p>Manager</p>
         <p>5XX, Elden Street</p>
         <p>Herndon, VA</p>
-        <p>+1 571 484 XXXX</p>
+        <p><MdCall /> +1 571 484 XXXX </p>
+        <div className="contact-div">
+                <a className="contact-list" href="https://www.twitter.com"><BsTwitter/>@tastebuds_tweet</a>
+                <a className="contact-list" href="https://www.fb.com"><FaFacebook/>tastebuds</a>
+                <a className="contact-list" href="https://www.instagram.com"><BsInstagram/>@tastebuds_gram</a>
+        </div>
     </div>
 
 )
