d113d3d815dc4f0921ceedd18a86c648a40f55e3
Ishwarya Lakshmi <ishwarya.lakshmi.b@gmail.com>
2022-05-06 00:04:07 -0400

    connectBE

--- src/components/RecipeDirectory/recipedirectory.components.jsx
+++ src/components/RecipeDirectory/recipedirectory.components.jsx
@@ -1,17 +1,40 @@
   
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Menuitem from "../Menu-item/menu-item.components";
 import './recipedirectory.styles.scss'
 import RECIPE_DATA from './recipe_data'
 // import SearchBar from "../searchBar/searchBar.component";
 // import NewSearch from "../searchBar/newSearch.component";
 import '../searchBar/searchBar.styles.scss'
+import axios from 'axios';
+
 const RecipeDirectory = () => {
-    const [recipes] = useState(RECIPE_DATA);
+    const [recipes, setRecipes] = useState(RECIPE_DATA);
     const [searchField, setField] = useState('');
+    const [data, setData] = useState({});
+    const [ setLoading] = useState(false);
+
     const handleChange = (event) =>{
         setField(event)
     }
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const { data: response } = await axios.get('http://localhost:5005/listings');
+            setData(response);
+            setRecipes(response)
+            console.log(data);
+            
+          } catch (error) {
+            console.error(error)
+          }
+          setLoading(false);
+        };
+    
+        fetchData();
+      }, []);
+      
     const filteredMonster = recipes.filter( item => item.title.toLowerCase().includes(searchField.toLocaleLowerCase()));
     console.log(filteredMonster)
     return (
